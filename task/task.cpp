687e7f59ccdd39ae32ebd9e3b966f67d060b6ed9
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-07-29 11:34:38 +0300

    Add project files.

---
+++ task/task.cpp
@@ -0,0 +1,105 @@
+﻿/*
+############################################ ЗАДАНИЕ ###########################################
+				Написать программу, позволяющую формировать список студентов академической группы.
+	Интерфейс программы должен предоставлять пользователю следующие возможности:
+	a. добавление студентов в группу;
+	b. удаление студента из группы;
+	c. модификация данных студента;
+	d. печать академической группы;
+	e. сортировка списка студентов;
+	f. поиск студента по заданному критерию;
+	g. сохранение списка студентов в файл (при завершении работы программы);
+	h. загрузка списка студентов из файла (при старте программы);
+	i. выход из программы.
+	При разработке программы следует использовать двумерный динамический массив для хранения списка
+студентов (одномерный динамический массив указателей на объекты класса Student).
+	Для реализации функциональности программы необходимо разработать следующие классы:
+	
+	class Person
+	{
+	protected:
+	char *name;
+	char *surname;
+	int age;
+	public:
+	Person();
+	Person(const char * name, const char * surname, int age);
+	Person(const Person &obj);
+	Person(Person &&obj);
+	~Person();
+	const char* getName()const;
+	const char * getSurname()const;
+	int getAge() const;
+	void setName(const char * name);
+	void setSurname(const char * surname);
+	void setAge(int age);
+	void Print()const;
+	Person& operator=(const Person &obj);
+	Person& operator=(Person &&obj);
+	friend std::istream& operator>>(std::istream &is, Person& obj);
+	friend std::ostream& operator<<(std::ostream &os, const Person& obj);
+	};
+
+	class Student : public Person
+	{
+	char phone[14];
+	double rating;
+	public:
+	Student();
+	Student(const char * name, const char * surname, int age, const char * phone,
+	double rating);
+	Student(const Student &obj);
+	Student(Student &&obj);
+	double getRating() const;
+	const char * getPhone() const;
+	void setPhone(const char * phone);
+	void setRating(double average);
+	void Print() const;
+	Student& operator=(const Student &obj);
+	Student& operator=(Student &&obj);
+	friend std::istream& operator>>(std::istream &is, Student& obj);
+	friend std::ostream& operator<<(std::ostream &os, const Student& obj);
+	};
+	
+	class AcademyGroup
+	{
+	Student ** pSt;
+	int count;
+	public:
+	AcademyGroup();
+	AcademyGroup(const AcademyGroup& obj);
+	AcademyGroup(AcademyGroup &&obj);
+	~AcademyGroup();
+	void AddStudents();
+	void DeleteStudent();
+	void EditStudent();
+	void Print();
+	void FindStudent();
+	void Sort();
+	void Save();
+	void Load();
+	AcademyGroup& operator=(const AcademyGroup &obj);
+	AcademyGroup& operator=(AcademyGroup &&obj);
+	Student& operator[](int index);
+	friend std::ostream& operator<<(std::ostream &os, AcademyGroup& obj);
+	};
+################################################################################################
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
